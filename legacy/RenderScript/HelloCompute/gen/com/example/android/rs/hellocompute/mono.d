5d06c6895d1d32e6c7412917691f73c6e764ab58
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2014-07-30 09:42:35 +0300

    Initial commit

---
+++ legacy/RenderScript/HelloCompute/gen/com/example/android/rs/hellocompute/mono.d
@@ -0,0 +1,19 @@
+/*___Generated_by_IDEA___*/
+
+D:\workspace\android-samples\legacy\RenderScript\HelloCompute\res\raw\mono.bc \
+  C:\Users\dmitrykolesnikovich\AppData\Local\Temp\android_renderscript_autogeneration4649853059739191300tmp\com\example\android\rs\hellocompute\ScriptC_mono.java: \
+  D:\workspace\android-samples\legacy\RenderScript\HelloCompute\src\com\example\android\rs\hellocompute\mono.rs \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include\rs_core.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_types.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\clang-include\stdbool.h \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_allocation.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_atomic.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_cl.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_debug.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_element.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_math.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_matrix.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_object.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_quaternion.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_sampler.rsh \
+  C:\Program\ Files\ (x86)\Android\android-sdk\build-tools\20.0.0\renderscript\include/rs_time.rsh
